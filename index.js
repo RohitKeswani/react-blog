a2c9bf4b7dfaf2c08d0e12bc770a32d81cb75223
RohitKeswani <rhareshk@gmail.com>
2020-05-18 22:04:43 -0400

    started authentication logic)

--- index.js
+++ index.js
@@ -16,15 +16,47 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.post('/api/users/register', (req, res)=>{
-        const user = new User(req.body)
-        user.save((err) => {
+        const user = new User(req.body);
+        
+        user.save((err, doc) => {
             if(err) return res.json({success: false, err})
             return res.status(200).json({
-                success: true
+                success: true,
+                userData: doc
             })
         })
 })
 
+
+app.post('/api/user/login', (req, res)=>{
+    //find email
+    User.findOne({email: req.body.email}, (err, user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess : false,
+                message: "Authentication failed, no email address found in database."
+            });
+        }
+        //compare password
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if(!isMatch){
+                return res.json({
+                    loginSuccess : false,
+                    message: "Authentication failed, password incorrect."
+                });
+            }
+            //generate token
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                .status(200).json({
+                    loginSuccess: true,
+                    message: "Authentication successful."
+                })
+            })
+        })
+    });
+})
 app.get('/',function (req, res){
     res.send("Hello, Rohit")
 });
